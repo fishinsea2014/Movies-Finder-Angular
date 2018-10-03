97a15ea243a51736a175af06db0106502f1e7826
Jason Qu <fishinsea2014@gmail.com>
2018-10-03 23:52:09 +1300

    Setup website framework with bootstrap.

--- Jason-MovieFinder/src/app/app.component.ts
+++ Jason-MovieFinder/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+interface ManuItem {
+  id: number;
+  name: string;
+}
+
+const ManuItems: ManuItem[] =[
+  {id:1, name:'Action'},
+  {id:2, name:'Adventure'},
+  {id:3, name:'Animation'},
+  {id:4, name:'Comedy'}
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Jason-MovieFinder';
+  genres= ManuItems;
 }
