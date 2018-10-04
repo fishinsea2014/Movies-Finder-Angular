d3870419630d1833af47ae534d9c102ead2c67dc
Jason Qu <fishinsea2014@gmail.com>
2018-10-04 14:42:29 +1300

    Complete setting up routes.

--- Jason-MovieFinder/src/app/app.module.ts
+++ Jason-MovieFinder/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {MoviesComponent} from './movies/movies.component';
+import {UpcomingComponent} from './upcoming/upcoming.component';
+import { appRoutes } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    UpcomingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
