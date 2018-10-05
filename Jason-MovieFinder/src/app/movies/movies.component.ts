1d01e47da335cc143afe6b9b4a58ec1195bca6ca
Jason Qu <fishinsea2014@gmail.com>
2018-10-06 01:56:41 +1300

    Display the top rated movies at the beginning.

--- Jason-MovieFinder/src/app/movies/movies.component.ts
+++ Jason-MovieFinder/src/app/movies/movies.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { MoviesService } from '../movies.service';
+import { MoviesService} from '../movies.service';
+
 
 @Component ({
     selector: "movies",
     templateUrl: "./movies.component.html",
-    styleUrls: ['./movies.component.css']
+    styleUrls: ['./movies.component.css'],
+    providers: [MoviesService]
 })
 
 export class MoviesComponent {
     searchStr: string;
+    topRatedMovies: Array<Object>;
+
+    constructor(private http:MoviesService){}
+
+    ngOnInit(){
+        this.http.getTopRatedMovies().subscribe(res =>{
+            this.topRatedMovies = res.results;
+        })
+    }
 
     searchMovies(){
         console.log("searching...")
